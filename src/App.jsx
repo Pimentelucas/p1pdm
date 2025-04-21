5cc17947943ba2227a8aa120b47238b6edaf6b84
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-04-21 19:50:10 -0300

    feat(p1): exibe lista ficticia

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import busca from './components/busca'
-
+import React from 'react';
+import Busca from './components/Busca';
+import LocalidadeLista from './components/LocalidadeLista';
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primeicons/primeicons.css'
 
 const App = () => {
+  const onBuscaRealizada = (cep) => {
+    console.log("CEP buscado:", cep);
+  };
+
   return (
-    <div>
-      <h1>
-        Hello P1
-      </h1>
-      <Busca/>
+    <div className="p-4">
+      <h2>Hello, P1</h2>
+      <Busca onBuscaRealizada={onBuscaRealizada} />
+      <LocalidadeLista />
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/LocalidadeLista.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card } from 'primereact/card';
+
+const LocalidadeLista = () => {
+  const localidades = [
+    {
+      cep: '04094-050',
+      endereco: 'Avenida Pedro Álvares Cabral',
+      bairro: 'Parque Ibirapuera',
+      cidade: 'São Paulo',
+      estado: 'SP'
+    },
+    {
+        cep: '55592-970',
+        endereco: 'Rua dos Navegantes',
+        bairro: 'Vila de Porto de Galinhas',
+        cidade: 'Ipojuca',
+        estado: 'PE'
+    }
+  ];
+
+  return (
+    <div className="grid gap-3 mt-4">
+      {localidades.map((local, index) => (
+        <Card key={index} title={local.cep} className="shadow-2">
+          <p>{local.endereco}</p>
+          <p>{local.bairro}</p>
+          <p>{local.cidade} - {local.estado}</p>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default LocalidadeLista;
+
