28ba9b4d6c820133152df9b92f295e252e8179b3
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-04-25 15:19:02 -0300

    feat(p1) ajusta grafico

--- src/components/GraficoEstados.jsx
+++ src/components/GraficoEstados.jsx
@@ -1,96 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Chart } from 'primereact/chart';
 
-export default class GraficoEstados extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      chartData: {
-        labels: [],
-        datasets: []
-      },
-      chartOptions: {
-        plugins: {
-          legend: {
-            labels: {
-              usePointStyle: true,
-              color: '#f1f1f1'
-            }
-          },
-          title: {
-            display: true,
-            text: 'Buscas por Estado (UF)',
-            font: {
-              size: 18
-            },
-            color: '#f1f1f1'
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    };
-  }
-
-  componentDidMount() {
-    this.atualizarGrafico(this.props.localidades);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.localidades !== this.props.localidades) {
-      this.atualizarGrafico(this.props.localidades);
-    }
-  }
-
-  contarEstados(localidades) {
+const GraficoEstados = ({ localidades }) => {
     const contagem = {};
+
     localidades.forEach(local => {
-      const estado = local.uf;
-      contagem[estado] = (contagem[estado] || 0) + 1;
-    });
-    return contagem;
-  }
+        const uf = local.uf;
+        if (uf) contagem[uf] = (contagem[uf] || 0) + 1;
+    })
 
-  atualizarGrafico(localidades) {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const estados = this.contarEstados(localidades);
-    const cores = [
-      '--orange-500', '--purple-500', '--teal-500',
-      '--indigo-500', '--lime-500', '--bluegray-500'
-    ];
-    const hoverCores = [
-      '--orange-400', '--purple-400', '--teal-400',
-      '--indigo-400', '--lime-400', '--bluegray-400'
-    ];
+    if (Object.keys(contagem).length === 0) return null;
 
-    this.setState({
-      chartData: {
-        labels: Object.keys(estados),
-        datasets: [
-          {
-            data: Object.values(estados),
-            backgroundColor: Object.keys(estados).map((_, i) =>
-              documentStyle.getPropertyValue(cores[i % cores.length])
-            ),
-            hoverBackgroundColor: Object.keys(estados).map((_, i) =>
-              documentStyle.getPropertyValue(hoverCores[i % hoverCores.length])
-            )
-          }
-        ]
-      }
-    });
-  }
+    const data = {
+      cores: [
+        '#A5D8FF', '#FFC9C9', '#D0BFFF', '#C5F6FA', '#FFE066',
+        '#B2F2BB', '#FFD8A8', '#F783AC', '#A9E34B', '#FF9F1C'
+      ],
+      HoverCores: [
+        '#74C0FC', '#FFA8A8', '#B197FC', '#99E9F2', '#FFD43B',
+        '#69DB7C', '#FFC078', '#F06595', '#94D82D', '#FF922B'
+      ]
+    }
 
-  render() {
+    const estados = Object.keys(contagem);
+
     return (
-      <div className="chart-wrapper h-96">
-        <Chart
-          type="pie"
-          data={this.state.chartData}
-          options={this.state.chartOptions}
-        />
-      </div>
+        <div className="flex justify-content-center w-full mt-4">
+            <div className="w-full md:w-30rem">
+                <Chart type="pie" data={{
+                    labels: estados,
+                    datasets: [{
+                        data: Object.values(contagem),
+                    }]
+                }} />
+            </div>
+        </div>
     );
-  }
-}
+};
+
+export default GraficoEstados;
